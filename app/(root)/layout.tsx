fd73f03fedb69a66e4739f234aaaf164ffa2df27
eAmartya <eageramartyacodingrn@gmail.com>
2024-11-04 18:09:31 +0530

    init-signinproblem

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <>
+      <Navbar />
+      <main className="font-work-sans">{children}</main>
+    </>
+  );
+};
+
+export default layout;
