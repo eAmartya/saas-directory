e72c429aad83d2fb55ec9e85b3a059ac56c3b0a6
eAmartya <eageramartyacodingrn@gmail.com>
2024-11-04 18:34:54 +0530

    signin-problemsolved

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,36 @@
-import { auth } from "@/auth";
-import { signIn, signOut } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
 
 const Navbar = async () => {
   const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src={"/logo.png"} alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
+
         <div className="flex items-center gap-5 text-black">
-          {session && session.user ? (
+          {session && session?.user ? (
             <>
-              <Link href={"/startup/create"}>
-                <span>Create</span>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
               </Link>
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                </button>
+              </form>
+
               <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
@@ -29,6 +39,7 @@ const Navbar = async () => {
             <form
               action={async () => {
                 "use server";
+
                 await signIn("github");
               }}
             >
