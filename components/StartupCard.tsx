15c7a0dd51e0aecc1aaed9d064397c7a52c600cb
eAmartya <eageramartyacodingrn@gmail.com>
2024-11-05 10:41:20 +0530

    sanity init checkpoint

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -21,9 +21,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       <div className="flex-between">
         <p className="startup_card_date">{formatDate(_createdAt)}</p>
         <div className="flex gap-1.5">
-          <EyeIcon className="size-6 text-primary">
-            <span className="text-16-medium">{views}</span>
-          </EyeIcon>
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
       <div className="flex-between mt-5 gap-5">
